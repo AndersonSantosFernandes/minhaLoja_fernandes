ef8d2a256d94a5bbea9a8454cf3c26a50231d507
Anderson <andersantfer@gmail.com>
2024-05-19 21:48:18 -0300

    Firs commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+export function App() {  
+
+  return (
+    <>
+     
+     <h1>Anderson Santos Fernandes</h1>
+     <h3>Aprendendo React JS</h3>
+    </>
+  )
+}
+
+
