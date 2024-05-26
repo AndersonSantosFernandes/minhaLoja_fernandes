78e3a05f1c107dc3e7f5f66abba06fc6cff005b1
Anderson <andersantfer@gmail.com>
2024-05-26 13:44:26 -0300

    botao com contador

--- src/components/count-item/count-item.jsx
+++ src/components/count-item/count-item.jsx
@@ -1,8 +1,24 @@
 
+import { useState } from 'react'
 import './count-item.scss'
 
-export function CountItem(){
-    return(
-        <p>5</p>
+export function CountItem() {
+
+    const [count, setCount] = useState(0) //Inicializa a variável com 0
+
+    const increment = () => setCount(count + 1) // função anonoma na arowFunction
+    const decrement = () => setCount(count - 1) // função anonoma na arowFunction
+    return (
+
+        <>
+        
+            <div className="add">
+                <button onClick={increment}>+</button>
+                <p>{count}</p>
+                <button onClick={decrement}>-</button>
+            </div>
+
+        </>
+
     )
 }
